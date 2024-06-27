b1f441fe9c97f232345eb1e82752c132f0dbb364
JuanaTib07 <juanitatiberti@gmail.com>
2024-06-27 09:23:52 -0300

    .

---
+++ Assets/PrecioComida.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrecioComida : MonoBehaviour
+{
+    public GameObject[] comida;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //random.SetRandomPosition();
+        //GameObject prefab = comida[Random.Range(0, comida.Length)];
+        //Instantiate(prefab, clonePoint.position, clonePoint.rotation);
+    }
+
+}
