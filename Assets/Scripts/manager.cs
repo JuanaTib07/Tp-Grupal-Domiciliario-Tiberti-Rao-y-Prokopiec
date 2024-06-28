c676584d4500c9638c0d9b3cbc1636a67c8e9db0
porfi2089 <108485835+porfi2089@users.noreply.github.com>
2024-06-28 00:50:06 -0300

    agregue movimiento estatico y arregle logica de spawn

--- Assets/Scripts/manager.cs
+++ Assets/Scripts/manager.cs
@@ -19,6 +19,10 @@ public class manager : MonoBehaviour
 
     public Text resultado;
 
+    public Toggle toggle1;
+    public Toggle toggle2;
+    public Toggle toggle3;
+
     public GameObject[] objetos;
 
     int resul;
@@ -27,16 +31,12 @@ public class manager : MonoBehaviour
     void Start()
     {
         valor1=Instantiate(objetos[Random.Range(0, objetos.Length)], spawnpoint.position, Quaternion.identity).GetComponent<PRODUCTO>().precio;
-        
+        textoprecio.text = valor1.ToString() + "$";
+
         int indexobj = Random.Range(0, objetos.Length);
         valor2 = objetos[indexobj].GetComponent<PRODUCTO>().precio;
         Instantiate(objetos[indexobj], spawnpoint1.position, Quaternion.identity);
         textoproducto1.text = valor2.ToString() + "$";
-
-        resul = valor2 + valor1;
-
-
-        textoprecio.text = resul.ToString();
         
         int a = Random.Range(0, objetos.Length);
         int valora= objetos[a].GetComponent<PRODUCTO>().precio;
@@ -46,9 +46,15 @@ public class manager : MonoBehaviour
         int b = Random.Range(0, objetos.Length);
         int valorb = objetos[b].GetComponent<PRODUCTO>().precio;
         Instantiate(objetos[b], spawnpoint3.position, Quaternion.identity);
-        textoproducto2.text = b.ToString() + "$";
+        textoproducto3.text = valorb.ToString() + "$";
 
+        resul = valor1 + (Random.Range(0.0f, 1.0f) < 0.5f ? valora : valorb);
+        resultado.text = resul.ToString()+"$";
 
+    }
 
+    void Resultado()
+    {
+        
     }
 }
---
+++ Assets/Scripts/objectMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectMovement : MonoBehaviour
+{
+    public float frequency = 2;
+    public float amplitude = 10;
+    public float rotation_speed = 3;
+    float random_offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        random_offset = Random.Range(0.0f, 50.0f);
+        transform.Rotate(new Vector3(0, Random.Range(0.0f, 360.0f), 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, Mathf.Sin(Time.fixedTime*frequency+random_offset)*Time.deltaTime*amplitude, 0));
+        transform.Rotate(new Vector3(0, rotation_speed * Time.deltaTime, 0));
+    }
+}
